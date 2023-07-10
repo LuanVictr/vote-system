89e61f7612fd72a1428bad4c6ebd90145027c8f0
LuanVictr <luanvictor2108@gmail.com>
2023-07-10 14:59:21 -0300

    Feat: Implementando classe abstrata pessoa e classe pessoa candidata | Req 01 e 02

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,17 @@
 package com.betrybe.sistemadevotacao;
 
-public class Pessoa {
-  
+public abstract class Pessoa {
+  protected String nome;
+
+  public Pessoa(String nome) {
+    this.nome = nome;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String novoNome) {
+    this.nome = novoNome;
+  }
 }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,38 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
 
+  /**
+   * Constroi a classe Pessoa candidata com o método de incrementacao
+   * de voto.
+   * @param nome recebe o nome do candidato
+   * @param numero recebe o numero do candidato
+   */
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return this.numero;
+  }
+
+  public int getVotos() {
+    return this.votos;
+  }
+
+  public void setNumero(int novoNumero) {
+    this.numero = novoNumero;
+  }
+
+  public void setVotos(int novosVotos) {
+    this.votos = novosVotos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
